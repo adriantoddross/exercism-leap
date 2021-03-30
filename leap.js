fedb7dd5db7f5730afe77671dd5edfd32924f610
Adrian Ross <adriantoddross@gmail.com>
2021-03-30 10:06:23 -0400

    Pass 7 out of 9 tests.

--- leap.js
+++ leap.js
@@ -9,7 +9,10 @@ export const isLeap = (year) => {
 
   if (year % 4 === 0 && year % 100 !== 0) {
     return true;
-  } else if (year % 100 !== 0 && year % 400 === 0) {
+  } else if (
+    (year % 100 !== 0 && year % 400 === 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  ) {
     return true;
   } else {
     return false;
